b84fd424b869258a6f537dc062660f55c191c28f
gregory10182 <Gregory10183@gmail.com>
2022-12-10 18:34:27 -0300

    Agregando Variables de entorno al puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 
 const server = http.createServer(app);
-
+const PORT = process.env.PORT || 8080 
 const { Server } = require('socket.io');
 const io = new Server(server);
 
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(8080, () => {
-    console.log('Server is running on port 8080');
+server.listen(PORT , () => {
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 });
 
